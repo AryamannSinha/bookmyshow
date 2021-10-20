a299f2749d8685987ecc483a86248ab6c89a0367
AryamannSinha <1930031@sliet.ac.in>
2021-10-20 23:12:33 +0530

    bookmyshow project

---
+++ src/config/TempPosters.config.js
@@ -0,0 +1,55 @@
+
+  const PremierImages = [
+    {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-sztaxpptsc-portrait.jpg",
+        alt: "zack",
+        title: "Zacl",
+        subtitle: "English",
+      },
+      {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122511-epjvjxmnab-portrait.jpg",
+        alt: "Godilla",
+        title: "Godzilla vs. Kong",
+        subtitle: "English",
+      },
+      {
+        src: "https://in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/pizza-a-love-story-et00312755-15-07-2021-09-47-07.jpg",
+        alt: "pizza",
+        title: "Pizza: A Love Story",
+        subtitle: "English",
+      },
+      {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00138770-nfmyhhxxyl-portrait.jpg",
+        alt: "TAILGATE",
+        title: "TAILGATE",
+        subtitle: "English",
+      },
+      {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-xxwtjnexzz-portrait.jpg",
+        alt: "PROXIMITY",
+        title: "PROXIMITY",
+        subtitle: "English",
+      },
+      {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00077622-yhtrswxwmr-portrait.jpg",
+        alt: "WONDER WOMEN 1984",
+        title: "WONDER WOMEN 1984",
+        subtitle: "English",
+      },
+      {
+        src: "https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00310901-zwfvttldbm-portrait.jpg",
+        alt: "AMMONITE",
+        title: "AMMONITE",
+        subtitle: "English",
+      },
+      {
+        src: "https://in.bmscdn.com/discovery-catalog/events/et00303783-lmvxvxzvrd-portrait.jpg",
+        alt: "The Tunnel",
+        title: "The Tunnel",
+        subtitle: "English",
+      },
+    ];
+
+
+
+export default PremierImages;
